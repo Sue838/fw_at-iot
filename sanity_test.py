8edc3b16da13d43c82c88bba21fa786c744b3bb0
Sue838 <suelight838@gmail.com>
2024-03-29 08:29:05 +0100

    Commit before switching to branch lecture16

--- sanity_test.py
+++ sanity_test.py
@@ -1,11 +1,10 @@
-def test_sanity(get_sensor_info, get_sensor_reading, get_sensor_methods, set_sensor_name, set_sensor_reading_interval, reset_sensor_to_factory, update_sensor_firmware, sensor_reboot):
+from time import sleep
+from requests.exceptions import JSONDecodeError
+
+
+def test_sanity(get_sensor_info, get_sensor_reading):
     sensor_info = get_sensor_info()
-    """"set_sensor_name("new-name")
-    sensor_methods = get_sensor_methods()
-    set_sensor_reading_interval("5")
-    reset_to_factory = reset_sensor_to_factory()
-    update_firmware = update_sensor_firmware()
-    reboot = sensor_reboot()"""
+    
     sensor_name = sensor_info.get("name")
     assert isinstance(sensor_name, str), "Sensor name is not a string"
 
@@ -26,3 +25,38 @@ def test_sanity(get_sensor_info, get_sensor_reading, get_sensor_methods, set_sen
 
     print("Sanity test passed")
 
+
+def test_reboot(get_sensor_info, sensor_reboot):
+    """
+    Steps:
+        1. Get original sensor info.
+        2. Reboot sensor.
+        3. Wait for sensor to come back online.
+        4. Get current sensor info.
+        5. Validate  that info from Step 1 is equal to info in Step 4. 
+    """
+print("Get original sensor info")
+sensor_info_before_reboot = get_sensor_info()
+
+print("Reboot sensor")
+reboot_response = sensor_reboot()
+assert reboot_response == "rebooting", "Sensor did not return proper text in response to reboot request"
+
+print("Wait for sensor to come back online")
+sensor_info_after_reboot = None
+
+for i in range(10):
+    print(f"Waiting for sensor; attempt (i+1)")
+    try:
+        print("Get current sensor info")
+        sensor_info_after_reboot = get_sensor_info()
+        break
+    except JSONDecodeError:
+        print("Couldn't get sensor info")
+        pass
+
+    print("Sleepinf for 1 second")
+    sleep(1)
+
+print("Validate  that info from Step 1 is equal to info in Step 4") 
+assert original_sensor_info == current_sensor_info, "Sensor info after reboot doesn't match sensor info before reboot"
